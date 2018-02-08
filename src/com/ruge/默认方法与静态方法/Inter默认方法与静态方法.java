e0f9c38c8aaf0fe7ab8ccb49b1296c6df526c882
wujian <12345678>
2018-02-08 20:37:24 +0800

    默认方法与静态方法

---
+++ src/com/ruge/默认方法与静态方法/Inter默认方法与静态方法.java
@@ -0,0 +1,16 @@
+package com.ruge.默认方法与静态方法;
+
+/**
+ * \* Created with IntelliJ IDEA.
+ * \* User: 武健
+ * \* Date: 2018/2/7
+ * \* Time: 22:15
+ * \* To change this template use File | Settings | File Templates.
+ * \* Description:
+ * \
+ */
+public interface Inter默认方法与静态方法 {
+default void getName(){
+    System.out.println("Inter默认方法与静态方法");
+}
+}
---
+++ src/com/ruge/默认方法与静态方法/Test.java
@@ -0,0 +1,18 @@
+package com.ruge.默认方法与静态方法;
+
+
+/**
+ * \* Created with IntelliJ IDEA.
+ * \* User: 武健
+ * \* Date: 2018/2/7
+ * \* Time: 22:17
+ * \* To change this template use File | Settings | File Templates.
+ * \* Description:
+ * \
+ */
+public class Test extends Class默认方法与静态方法 implements Inter默认方法与静态方法 {
+    public static void main(String[] args) {
+        Test test = new Test();
+        test.getName();
+    }
+}
